73d1e0239fab3e54651a485088d4732c428a1d5a
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2020-06-15 17:25:34 +0100

    inital commit, beggining to create the navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {
+    AppBar,
+    Toolbar,
+    ListItem,
+    IconButton,
+    ListItemText,
+    Avatar,
+    Divider,
+    List,
+    Typography,
+    Box
+} from "@material-ui/core";
+import {
+    ArrowBack,
+    AssignmentInd,
+    Home,
+    Apps,
+    ContactMail
+} from "@material-ui/icons";
+
+export const NavBar = () => {
+    return (
+        <Box component="nav">
+            <AppBar>
+                <Toolbar>
+                    <ArrowBack />
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+};
---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Home = () => {
+    return (
+        <div>
+            <h2>This is a Home component</h2>
+        </div>
+    );
+};
