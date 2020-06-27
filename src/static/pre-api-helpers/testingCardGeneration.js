d8fad6699e68fc2d3612ec9505db55a908b3f8cb
JoaquimXG <49433786+JoaquimXG@users.noreply.github.com>
2020-06-27 21:23:43 +0100

    created template for asset card generation, need to write function to parse the template given an asset from the database

--- src/static/pre-api-helpers/testingCardGeneration.js
+++ src/static/pre-api-helpers/testingCardGeneration.js
@@ -1,5 +1,18 @@
-export const cardTemplate = {
-    title: "Important Items",
+const contentTemplate = {
+    subtitle: "",
+    content: ""
+};
+
+const cardTemplate = {
+    title: "",
+    content: [contentTemplate, contentTemplate],
+    columns: {
+        xs: 12
+    }
+};
+
+export const cardsTemplate = {
+    header: "",
 
     cards: [
         {
@@ -7,33 +20,68 @@ export const cardTemplate = {
             content: [
                 {
                     title: "",
-                    content:
-                        ""
+                    content: ""
                 }
             ],
             columns: {
                 xs: 12
             }
         },
+        cardTemplate
+    ]
+};
 
+export const assetsTemplate = {
+    header: "name",
+
+    cards: [
         {
-            title: "",
-            content: [
-                {
-                    title: "",
-                    content: ""
-                },
-                {
-                    title: "",
-                    content: ""
-                }
-            ],
+            title: "Networking",
+            content: ["internal_ip", "external_ip", "hostname", "domain"],
             columns: {
                 xs: 4
             }
         },
+        {
+            title: "Usage",
+            content: ["creation_date", "last_user", "last_audit", "os"],
+            columns: {
+                xs: 4
+            }
+        },
+        {
+            title: "Technical",
+            content: ["type", "model", "serial", "os"],
+            columns: {
+                xs: 4
+            }
+        },
+        {
+            title: "notes",
+            content: ["notes"],
+            columns: {
+                xs: 12
+            }
+        },
+        {
+            title: "notes",
+            content: [
+                "udf1",
+                "udf2",
+                "udf3",
+                "udf4",
+                "udf5",
+                "udf6",
+                "udf7",
+                "udf8",
+                "udf9",
+                "udf10",
+                "udf11",
+                "udf12"
+            ],
+            columns: {
+                xs: 4
+            }
+        }
     ]
 };
-
-
-// need to 
